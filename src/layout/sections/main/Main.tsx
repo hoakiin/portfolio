da86fad0ab1335b8b78834b64e1101d1bffe9591
hoakiin <millychak89@gmail.com>
2025-11-05 18:23:36 +0300

    feat: add Main component

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import photo from "../../../assets/images/photo.svg";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Main = () => {
+  return (
+    <StyledMain>
+      <FlexWrapper>
+        <div>
+          <MainTitle>WEB DEVELOPER</MainTitle>
+          <Name>Erica Font</Name>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt.
+          </p>
+        </div>
+
+        <img src={photo} alt="photo" />
+      </FlexWrapper>
+    </StyledMain>
+  );
+};
+
+const StyledMain = styled.div`
+    background-color: #bcecd5;
+    min-height: 100vh;
+`
+
+const MainTitle = styled.h1`
+   
+`
+
+const Name = styled.h2`
+   
+`
