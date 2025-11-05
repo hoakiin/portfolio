f44f0ea7c2ee4571b4f9c5144cab91dc3206fce1
hoakiin <millychak89@gmail.com>
2025-11-05 16:35:30 +0300

    feat: add Icon component

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,16 @@
+import iconsSprite from '../../assets/images/icons-sprite.svg'
+
+type IconPropsType = {
+    iconId: string;
+    width?: string;
+    height?: string;
+    viewBox?: string;
+}
+
+export const Icon = (props: IconPropsType) => {
+    return (
+        <svg width={props.width || "120"}  height={props.height || "120"} viewBox={props.width || "0 0 116 120"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${iconsSprite}#${props.iconId}`} />
+        </svg>
+    )
+}
