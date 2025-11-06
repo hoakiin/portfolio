bdf289dc4226bd76f7c0672f89305e4d414d97db
hoakiin <millychak89@gmail.com>
2025-11-06 17:37:36 +0300

    feat: add Tags component for Projects

---
+++ src/layout/sections/works/tags/Tags.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Tags = (props: { tagsItems: Array<string> }) => {
+  return (
+    <StyledTags>
+      <ul>
+        {props.tagsItems.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+    </StyledTags>
+  );
+};
+
+const StyledTags = styled.div`
+  ul {
+    display: flex;
+    gap: 10px;
+  }
+`;
