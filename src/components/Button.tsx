b75863fe66bcc5791e2862fb5fe305c243541d42
hoakiin <millychak89@gmail.com>
2025-11-13 16:55:53 +0300

    feat: add Button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 export const Button = styled.button`
-    
+    padding: 11px 18px;
+    border-radius: 6px;
+    background: ${theme.colors.gradient};
+    color: ${theme.colors.fontDark};
+    font-weight: 500;
 `
