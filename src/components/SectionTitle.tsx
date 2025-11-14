077fe341f53ee2614f8f1016624750076a360b20
hoakiin <millychak89@gmail.com>
2025-11-14 15:48:08 +0300

    feat: add more SectionTitle styles

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -2,5 +2,7 @@ import styled from "styled-components";
 import { theme } from "../styles/Theme";
 
 export const SectionTitle = styled.h2`
+  font-size: 24px;
+  font-weight: 500;
   color: ${theme.colors.title};
 `;
