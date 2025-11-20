163178189c95eade3da42ff425634732a2f0e11c
hoakiin <millychak89@gmail.com>
2025-11-20 18:59:21 +0300

    feat: add Languages to About section

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -1,49 +1,69 @@
 import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Container } from "../../../components/Container";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+import image from "../../../assets/images/about.png";
+import { Language } from "./language/Language";
 
 export const About = () => {
   return (
     <StyledAbout>
       <Container>
-        <SectionTitle>About me</SectionTitle>
-        <Text>
-          The long barrow was built on land previously inhabited in the
-          Mesolithic period. It consisted of a sub-rectangular earthen tumulus,
-          estimated to have been 15 metres (50 feet) in length, with a chamber
-          built from sarsen megaliths on its eastern end. Both inhumed and
-          cremated human remains were placed within this chamber during the
-          Neolithic period, representing at least nine or ten individuals.
-        </Text>
+        <FlexWrapper>
+          <Image src={image} />
+          <Information>
+            <SectionTitle>About me</SectionTitle>
+            <Text>
+              The long barrow was built on land previously inhabited in the
+              Mesolithic period. It consisted of a sub-rectangular earthen
+              tumulus, estimated to have been 15 metres (50 feet) in length,
+              with a chamber built from sarsen megaliths on its eastern end.
+              Both inhumed and cremated human remains were placed within this
+              chamber during the Neolithic period, representing at least nine or
+              ten individuals.
+            </Text>
+            <Languages>
+              <Language name="Russian" percent={100} level="Native" />
+              <Language name="English" percent={60} level="B1" />
+              <Language name="German" percent={40} level="A2" />
+            </Languages>
+          </Information>
+        </FlexWrapper>
       </Container>
     </StyledAbout>
   );
 };
 
 const StyledAbout = styled.section`
-  min-height: 60vh;
+  min-height: 80vh;
   display: flex;
   align-items: end;
+  margin-bottom: 78px;
+
+  ${FlexWrapper} {
+    gap: 50px;
+  }
+`;
+
+const Image = styled.img`
+  width: 330px;
+  height: 470px;
+`;
+
+const Information = styled.div`
+  padding: 10px 0;
 `;
 
 const Text = styled.p`
-  max-width: 800px;
+  max-width: 600px;
   font-size: 18px;
   line-height: 26px;
   letter-spacing: 0.1px;
-  margin: 0 0 130px 0;
+  margin: 0 0 30px 0;
+`;
 
-  position: relative;
-
-  &::before {
-    content: "";
-    position: absolute;
-    transform: translateY(-50%);
-    top: 50%;
-    left: -20px;
-    width: 5px;
-    height: 90%;
-    background: linear-gradient(360deg,#A1C4FD 0%, #C2E9FB 100%);
-    border-radius: 2px;
-  }
+const Languages = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
 `;
