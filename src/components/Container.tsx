04a18c1960dc136a4ff594f2f492804f422a1fce
hoakiin <millychak89@gmail.com>
2025-11-24 21:02:39 +0300

    fix: adjust paddings in Container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 export const Container = styled.div`
-    max-width: 1100px;
-    width: 100%;
-    min-height: 100%;
+  max-width: 1100px;
+  width: 100%;
+  min-height: 100%;
+  padding: 0 30px;
+  margin: 0 auto;
+  
+  @media ${theme.media.mobile} {
     padding: 0 15px;
-    margin: 0 auto;
-`
+  }
+`;
